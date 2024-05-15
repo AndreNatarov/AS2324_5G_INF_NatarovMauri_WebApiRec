f290eb36c8e76cab25db079ec7f65b8a718eadde
AndreNatarov <andrenatarovmauri@gmail.com>
2024-05-15 08:38:46 +0200

    feat: Creazione api per volume e ramo del vero

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -5,8 +5,21 @@ namespace AS2324_5G_INF_NatarovMauri_WebApiRec.Controllers
     public class MathController : Controller
     {
         //API per conversione da Celsius a Fahrenheit
-        
-        //API calcolo volume cilindro
 
+        //API calcolo volume cilindro
+        [HttpGet("GetVolumeCilindro")]
+        public JsonResult GetVolumeCilindro(double raggio, double altezza)
+        {
+            if (raggio > 0 && altezza > 0)
+            {
+                return Json(new
+                {
+                    risultato = Math.PI*raggio*raggio*altezza,
+                    message = "",
+                    status = "OK"
+                });
+            }
+            
+        }
     }
 }
