fc7fd89392305768787e6b91a863d8526f5cb51f
AndreNatarov <andrenatarovmauri@gmail.com>
2024-05-15 08:34:07 +0200

    Feat: Aggiunta di commenti per aiutare nello sviluppo

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -4,6 +4,9 @@ namespace AS2324_5G_INF_NatarovMauri_WebApiRec.Controllers
 {
     public class MathController : Controller
     {
-         
+        //API per conversione da Celsius a Fahrenheit
+        
+        //API calcolo volume cilindro
+
     }
 }
